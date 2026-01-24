d4d0ffee27be42de95489120fe3e73656b06f477
AHSAINI <happysain4522@gmail.com>
2026-01-24 18:09:14 +0530

    Update

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,20 +1,25 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, html }) => {
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // MUST be false for 587
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS, // Gmail App Password
+    },
+    tls: {
+      rejectUnauthorized: false, // prevents Render TLS timeout
+    },
+  });
 
-    await transporter.sendMail({
-        from: `"JOB Portal" <${process.env.EMAIL_USER}>`,
-        to,
-        subject,
-        html
-    });
+  await transporter.sendMail({
+    from: `"Job Portal" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  });
 };
 
 module.exports = sendEmail;
