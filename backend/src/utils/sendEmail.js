796ae6d8dbfbf3201f7b25d1f114bfcd22eae852
AHSAINI <happysain4522@gmail.com>
2026-01-24 19:12:15 +0530

    Update

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -3,11 +3,15 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
-  secure: false,
+  secure: false, // IMPORTANT
   auth: {
     user: process.env.BREVO_SMTP_USER,
     pass: process.env.BREVO_SMTP_KEY,
   },
+  tls: {
+    rejectUnauthorized: false, // IMPORTANT for Render
+  },
+  connectionTimeout: 10000, // 10 seconds
 });
 
 const sendEmail = async ({ to, subject, html }) => {
