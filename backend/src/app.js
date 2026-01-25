8524df254ba5ee432432e9380056203de9937937
AHSAINI <happysain4522@gmail.com>
2026-01-25 14:42:42 +0530

    Update

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // IMPORTANT for preflight
+// app.options("*", cors(corsOptions)); // IMPORTANT for preflight
 
 // ================= BODY PARSER =================
 app.use(express.json());
