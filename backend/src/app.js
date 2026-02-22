8d3bfd3b97f885fe22c004b8f47d58e0b4e12d5e
AHSAINI <happysain4522@gmail.com>
2026-02-22 19:59:55 +0530

    Commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,7 +21,8 @@ app.use(cors(corsOptions));
 
 // ================= BODY PARSER =================
 app.use(express.json());
-
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 // ================= ROUTES =================
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/profile", require("./routes/profile.routes"));
