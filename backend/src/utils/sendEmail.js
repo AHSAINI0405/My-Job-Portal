14220870485651173940f63a30625e21c8d5f0a6
AHSAINI <happysain4522@gmail.com>
2026-03-03 10:29:44 +0530

    commit

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,55 +1,19 @@
-// const emailjs = require("@emailjs/nodejs");
+const sgMail = require("@sendgrid/mail");
 
-// const sendEmail = async ({ to, subject, html }) => {
-//   try {
-//     await emailjs.send(
-//       process.env.EMAILJS_SERVICE_ID,
-//       process.env.EMAILJS_TEMPLATE_ID,
-//       {
-//         to_email: to,
-//         subject: subject,
-//         message: html,
-//       },
-//       {
-//         publicKey: process.env.EMAILJS_PUBLIC_KEY,
-//         privateKey: process.env.EMAILJS_PRIVATE_KEY,
-//       }
-//     );
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-//     console.log("Email sent successfully");
-//   } catch (error) {
-//     console.error("EmailJS Error:", error);
-//     throw error;
-//   }
-// };
-
-// module.exports = sendEmail;
-
-
-
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false, // IMPORTANT
-  auth: {
-    user: process.env.BREVO_SMTP_USER,
-    pass: process.env.BREVO_SMTP_KEY,
-  },
-  tls: {
-    rejectUnauthorized: false, // IMPORTANT for Render
-  },
-  connectionTimeout: 10000, // 10 seconds
-});
-
-const sendEmail = async ({ to, subject, html }) => {
-  await transporter.sendMail({
-    from: `"Job Portal" <${process.env.BREVO_SMTP_USER}>`,
-    to,
-    subject,
-    html,
-  });
+const sendEmail = async (to, subject, html) => {
+  try {
+    await sgMail.send({
+      to,
+      from: process.env.email,
+      subject,
+      html,
+    });
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error(error.response?.body || error.message);
+  }
 };
 
 module.exports = sendEmail;
