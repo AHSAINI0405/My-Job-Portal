ebbf7a1c3d33e99b071fd32ebe3085c0af9dd8b3
AHSAINI <happysain4522@gmail.com>
2026-03-03 11:35:27 +0530

    commit

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const auth = require("../controllers/auth.controller");
 
 router.post("/register", auth.register);
-router.post("/verify-otp", auth.verifyOTP);
+router.post("/verify-otp", auth.verifyOtp);
 router.post("/login", auth.login);
 router.post("/forgot-password", auth.forgotPassword);
 router.post("/reset-password",auth.resetPassword);
